e1800604c8928ce71f3aad1ab0fdd88dbc8909e9
Osama El Nagy <oelnagy@turndigital.net>
2023-10-24 16:06:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace SOLID_PRINCIPLES.LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //No Liskov Principle
+            Banana banana = new Strawberry();
+            Console.WriteLine($"Without Liskov--> Color of Banana: {banana.GetColor()}");
+
+            // With Liskov Principle
+            IFruit fruit = new Orange();
+            Console.WriteLine($"With Liskov--> Color of Orange: {fruit.GetColor()}");
+            fruit = new Apple();
+            Console.WriteLine($"With Liskov--> Color of Apple: {fruit.GetColor()}");
+            Console.ReadKey();
+        }
+    }
+
+    public class Banana
+    {
+        public virtual string GetColor()
+        {
+            return "Yellow";
+        }
+    }
+    public class Strawberry : Banana
+    {
+        public override string GetColor()
+        {
+            return "Red";
+        }
+    }
+
+
+    public interface IFruit
+    {
+        string GetColor();
+    }  
+ 
+    public class Apple : IFruit
+    {
+        public string GetColor()
+        {
+            return "Red";
+        }
+    }
+    public class Orange : IFruit
+    {
+        public string GetColor()
+        {
+            return "Orange";
+        }
+    }
+
+
+   
+}
